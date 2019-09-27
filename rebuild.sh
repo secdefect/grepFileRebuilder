188bda20c9848caaf3fbea65e70fd79c0f379647
secdefect <sash@modux.co.uk>
2019-09-27 12:37:48 +0100

    Create rebuild.sh

---
+++ rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#grep file rebuilder
+#sash rigby
+#usage ./rebuild.sh inputfile
+file="$1"
+IFS=$'\n'
+lie=""
+
+for i in `grep ':' file | sed 's/:/¬/'`
+do
+echo $i
+IFS='¬' read -r -a array <<< "$i"
+
+test -f "${array[0]}" || install -D <(echo )  "${array[0]}"
+echo -e "${array[1]}" >> "${array[0]}"
+echo "${array[1]}"
+IFS=$'\n'
+done
+
+
+
